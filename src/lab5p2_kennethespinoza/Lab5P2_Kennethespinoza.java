e61ca34188d4ea2d8000413c10b6575e7140f0d8
KennethEspinoza14 <116386154+KennethEspinoza14@users.noreply.github.com>
2022-11-11 13:39:01 -0600

    Agregando clases

--- src/lab5p2_kennethespinoza/Lab5P2_Kennethespinoza.java
+++ src/lab5p2_kennethespinoza/Lab5P2_Kennethespinoza.java
@@ -1,21 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab5p2_kennethespinoza;
 
-/**
- *
- * @author Kenneth
- */
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Random;
+
 public class Lab5P2_Kennethespinoza {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner lea = new Scanner(System.in);
+    static Random r = new Random();
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        ArrayList<Robots> robot = new ArrayList();
+
+        boolean control = true;
+
+        while (control == true) {            
+            
+            System.out.println("----------------MENU-----------------");
+            System.out.println("|1| =  Crear Robot");
+            System.out.println("|2| =  Listar Robot");
+            System.out.println("|3| =  Simular Automaticamente");
+            System.out.println("|4| =  Actuar Manualmente");
+            System.out.println("|6| = Salir del programa ");
+            System.out.println("-------------------------------------");
+            System.out.println("Ingrese la opcion a la que desea entrar:");
+            int opcion = lea.nextInt();
+            
+            switch (opcion){
+                
+                case 1:{
+                    
+                }
+                break;
+                 
+                case 2:{
+                    
+                }
+                break;
+                 
+                case 3:{
+                    
+                }
+                break;
+                 
+                case 4:{
+                    
+                }
+                break;
+                 
+                case 5:{
+                    control = false;
+                }
+                break;
+                default:
+                    System.out.println("Opcion ingresada no valida");
+                
+            }
+            
+        }
+        
+        
+        
+        
+        
+        
+        
     }
     
 }
---
+++ src/lab5p2_kennethespinoza/robots_agiles.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5p2_kennethespinoza;
+
+/**
+ *
+ * @author Kenneth
+ */
+public class robots_agiles extends Robots{
+    
+}
